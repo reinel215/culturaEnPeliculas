68f14458a21596465bca8a871bdf19dddc71e724
Ezequiel <ezequielmonteroz8@gmail.com>
2020-11-13 11:35:18 -0400

    arreglo control errores funcion registro_usuario

---
+++ scripts-bd/funciones.sql
@@ -0,0 +1,23 @@
+select registrar_usuario('eeze','dss','dss','F','dss','dss')
+
+DROP FUNCTION registrar_usuario
+CREATE OR REPLACE FUNCTION registrar_usuario(
+    _usuario     VARCHAR(20),
+    _correo      VARCHAR(30),
+    _nombre      VARCHAR(20),
+    _genero      VARCHAR(1),
+    _contraseña  VARCHAR(20),
+    _ruta_imagen VARCHAR
+) RETURNS VARCHAR AS $$
+DECLARE
+BEGIN
+	INSERT INTO USUARIO(usuario,correo,nombre,genero,contraseña,ruta_imagen) 
+	VALUES(_usuario,_correo,_nombre,_genero,_contraseña,_ruta_imagen);
+	RETURN 'REGISTRO DE USUARIO EXITOSO';
+EXCEPTION
+	WHEN unique_violation THEN
+		RETURN 'ERROR REGISTRO DUPLICADO';
+    WHEN OTHERS THEN
+        RETURN 'ERROR INESPERADO';
+END;
+$$ LANGUAGE plpgsql;
