60ac377383f935d25028b25c3104b1011a60d3d6
Ezequiel <ezequielmonteroz8@gmail.com>
2020-12-28 15:43:36 -0400

    Arreglo registro usuario

--- scripts-bd/funciones.sql
+++ scripts-bd/funciones.sql
@@ -12,10 +12,41 @@ BEGIN
 	INSERT INTO USUARIO(usuario,correo,nombre,genero,contraseña,ruta_imagen) 
 	VALUES(_usuario,_correo,_nombre,_genero,_contraseña,_ruta_imagen);
 	RETURN 'REGISTRO DE USUARIO EXITOSO';
-EXCEPTION
-	WHEN unique_violation THEN
-		RETURN 'ERROR REGISTRO DUPLICADO';
-    WHEN OTHERS THEN
-        RETURN 'ERROR INESPERADO';
+END;
+$$ LANGUAGE plpgsql;
+
+DROP FUNCTION IF EXISTS registrar_pelicula;
+CREATE OR REPLACE FUNCTION registrar_pelicula(
+    _clave VARCHAR
+) RETURNS INTEGER AS $$
+DECLARE
+_id INTEGER;
+BEGIN
+    SELECT id INTO _id FROM PELICULA WHERE clave = _clave;
+    IF NOT FOUND THEN
+        INSERT INTO PELICULA(clave) VALUES(_clave);
+		SELECT id INTO _id FROM PELICULA WHERE clave = _clave;
+    END IF;
+	RETURN _id;
+END;
+$$ LANGUAGE plpgsql;
+
+DROP FUNCTION IF EXISTS registrar_critica;
+CREATE OR REPLACE FUNCTION registrar_critica(
+    _mensaje       VARCHAR,
+    pelicula_clave VARCHAR,
+    _usuario       VARCHAR(20)
+) RETURNS VARCHAR AS $$
+DECLARE
+    pelicula_id INTEGER;
+    usuario_id  INTEGER;
+BEGIN
+    SELECT registrar_pelicula(pelicula_clave) INTO pelicula_id;
+	SELECT id INTO usuario_id FROM USUARIO WHERE usuario = _usuario;
+
+	INSERT INTO CRITICA(mensaje, pelicula_id, usuario_id) 
+	VALUES(_mensaje, pelicula_id, usuario_id);
+	
+	RETURN 'REGISTRO DE USUARIO EXITOSO';
 END;
 $$ LANGUAGE plpgsql;
